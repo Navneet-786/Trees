cc26a81c4b5f3efd1066fe54203c0ce712226e05
Navneet verma <vermamanu52528591@gmail.com>
2026-01-18 20:23:43 +0530

    Update main.cpp

--- Basics/BST/main.cpp
+++ Basics/BST/main.cpp
@@ -21,4 +21,22 @@ int main()
 
   Node *root = createBST(vec);
   print(root);
+
+  cout << "wanna delete any element\n1->yes\n0->No" << endl;
+  bool res;
+  cin >> res;
+  if (!res)
+  {
+    cout << "No element deleted" << endl;
+    return 0;
+  }
+  int ele;
+  cout << "Enter the node that you wanna delete" << endl;
+  cin >> ele;
+
+  deleteNode(root, ele);
+
+  cout << "new tree will be: " << endl;
+  print(root);
+
 }
